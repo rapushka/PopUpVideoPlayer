d1930ab3a6b073493e3223f3839b78b2e77a38c1
Rapushka <nikolay.syatkovskiy@outlook.com>
2022-07-05 13:08:42 +0300

    Спроба рэялізаваць MVVM лапкамі

---
+++ VideoPlayer/ViewModels/MainViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using VideoPlayer.Models;
+
+namespace VideoPlayer.ViewModels;
+internal class MainViewModel : INotifyPropertyChanged
+{
+	private const string EmptyString = "";
+	private string _path;
+
+	public event PropertyChangedEventHandler? PropertyChanged;
+
+	public MainViewModel()
+	{
+		_path = @"C:\Самазанятак\[ЗАРАЗ] Архитектура мобильных игр на UNITY для профессионалов\01\Input. 2 Bootstrapper.mp4";
+	}
+
+	public string Path
+	{
+		get => _path;
+		set
+		{
+			_path = value;
+			OnPropertyChanged();
+		}
+	}
+
+	public ICommand PathAdd
+	{
+		get
+		{
+			return new FileOpener((obj) =>
+			{
+				Path = obj?.ToString()
+					?? throw new NullReferenceException();
+			});
+		}
+	}
+	public void OnPropertyChanged([CallerMemberName] string prop = EmptyString)
+	{
+		PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+	}
+}
---
+++ VideoPlayer/Views/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using VideoPlayer.ViewModels;
+
+namespace VideoPlayer.Views;
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+	public MainWindow()
+	{
+		InitializeComponent();
+	}
+}
