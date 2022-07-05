9b2c91935ff5d03fc72acb4e732359c8d544cb22
Rapushka <nikolay.syatkovskiy@outlook.com>
2022-07-05 17:08:00 +0300

    Відэя адчыняецца калі ўпісаць шлях

---
+++ VideoPlayer/Models/CommandDelegator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace VideoPlayer.Models;
+public class CommandDelegator<T> : ICommand
+	where T : class
+{
+	private readonly Action<T?> _excecute;
+	private readonly Func<T?, bool> _canExecute;
+
+	public CommandDelegator(Action<T?> excecute, Func<T?, bool> canExecute)
+	{
+		_excecute = excecute;
+		_canExecute = canExecute;
+	}
+
+	public CommandDelegator(Action<T?> excecute)
+		: this(excecute, (t) => true) { }
+
+	public event EventHandler? CanExecuteChanged
+	{
+		add => CommandManager.RequerySuggested += value;
+		remove => CommandManager.RequerySuggested -= value;
+	}
+
+	public bool CanExecute(object? parameter)
+	{
+		return parameter switch
+		{
+			null => _canExecute.Invoke(null),
+			T t => _canExecute.Invoke(t),
+			_ => throw new ArgumentException($"Invalid {nameof(parameter)} type")
+		};
+	}
+
+	public void Execute(object? parameter)
+	{
+		_excecute.Invoke(parameter as T);
+	}
+}
--- VideoPlayer/ViewModels/MainViewModel.cs
+++ VideoPlayer/ViewModels/MainViewModel.cs
@@ -1,45 +1,43 @@
 ﻿using System;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
+using DevExpress.Mvvm;
 using System.Windows.Input;
 using VideoPlayer.Models;
 
 namespace VideoPlayer.ViewModels;
-internal class MainViewModel : INotifyPropertyChanged
+public class MainViewModel : ViewModelBase
 {
-	private const string EmptyString = "";
+	private const string TestPath = 
+		@"C:\Самазанятак\[ЗАРАЗ] Архитектура мобильных игр на UNITY для профессионалов\01\Input. 1 Intro.mp4";
 	private string _path;
-
-	public event PropertyChangedEventHandler? PropertyChanged;
+	private string _input;
 
 	public MainViewModel()
 	{
-		_path = @"C:\Самазанятак\[ЗАРАЗ] Архитектура мобильных игр на UNITY для профессионалов\01\Input. 2 Bootstrapper.mp4";
+		_input = TestPath;
+		_path = string.Empty;
 	}
 
+	public string Input
+	{
+		get => _input;
+		set
+		{
+			_input = value;
+			RaisePropertyChanged(() => Input);
+		}
+	}
+	
 	public string Path
 	{
 		get => _path;
 		set
 		{
 			_path = value;
-			OnPropertyChanged();
+			RaisePropertyChanged(() => Path);
 		}
 	}
 
-	public ICommand PathAdd
-	{
-		get
-		{
-			return new FileOpener((obj) =>
-			{
-				Path = obj?.ToString()
-					?? throw new NullReferenceException();
-			});
-		}
-	}
-	public void OnPropertyChanged([CallerMemberName] string prop = EmptyString)
-	{
-		PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
-	}
+	public ICommand SetInputAsPath
+		=> new CommandDelegator<object>((x)
+			=> Path = Input);
 }
